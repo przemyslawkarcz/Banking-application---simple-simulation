8062e2e357c32cd06da17deadaa086f30c688223
Karcz Przemysław <pkarc@PGI.LOCAL>
2022-04-28 13:44:07 +0200

    the entire beginning of the program has been rebuilt

--- src/main/java/Customer_Bank.java
+++ src/main/java/Customer_Bank.java
@@ -62,4 +62,69 @@ public class Customer_Bank {
 
     }
 
+    public void find_customer_and_perform_operation_deposit_withdrawal_on_account(String id, String funds){
+
+        System.out.println("\n* * * List of customers and funds in the accounts * * *");
+
+        ArrayList<Object> bank_customer_list = Bank.getBank_customer_list();
+
+        Customer customer_ = new Customer();
+        customer_.setAccount_funds(funds);
+
+        Iterator<Object> iterator = bank_customer_list.iterator();
+        while (iterator.hasNext()){
+            String [] next = (String []) iterator.next();
+
+            if (next[0].equals(id)){
+
+                next[3] = customer_.getAccount_funds();
+
+                System.out.println(
+                        "Customer: " +
+                                "id='" + next[0] + '\'' +
+                                ", full_name='" + next[1] + '\'' +
+                                ", account_number='" + next[2] + '\'' +
+                                ", account_funds='" + next[3] + '\'' +
+                                ", other_data='" + next[4] + '\''
+                );
+
+            }
+
+        }
+
+    }
+
+    /*public void make_transfer_operation_between_customers(String id_client_ordering_transfer, String funds,
+                                                          String id_client_receiving_transfer){
+
+        System.out.println("\n* * * List of customers and funds after transfer operation * * *");
+
+        ArrayList<Object> bank_customer_list = Bank.getBank_customer_list();
+
+        Customer customer_ = new Customer();
+        customer_.setAccount_funds(funds);
+
+        Iterator<Object> iterator = bank_customer_list.iterator();
+        while (iterator.hasNext()){
+            String [] next = (String []) iterator.next();
+
+            if (next[0].equals(id_client_ordering_transfer)){
+
+                next[3] = customer_.getAccount_funds();
+
+                System.out.println(
+                        "Customer: " +
+                                "id='" + next[0] + '\'' +
+                                ", full_name='" + next[1] + '\'' +
+                                ", account_number='" + next[2] + '\'' +
+                                ", account_funds='" + next[3] + '\'' +
+                                ", other_data='" + next[4] + '\''
+                );
+
+            }
+
+        }
+
+    }*/
+
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,31 +4,42 @@ public class Main {
 
         // 1. creating new customers of bank and adding them to bank's list of customers,
         // we find the printouts on the 'List of all bank customers'
-        Customer customer_ = new Customer();
-        customer_.add_new_customer("1", "F. First");
-        customer_.add_new_customer("2", "S. Second");
-        customer_.add_new_customer("3", "T. Third");
-        customer_.add_new_customer("4", "Fo. Fourth");
-        customer_.add_new_customer("5", "Fi. Fifth");
+        Customer customer = new Customer();
+        customer.add_new_customer("1", "F. First");
+        customer.add_new_customer("2", "S. Second");
+        customer.add_new_customer("3", "T. Third");
+        customer.add_new_customer("4", "Fo. Fourth");
+        customer.add_new_customer("5", "Fi. Fifth");
 
         // 2. after creating many customers we try to search them in 'database',
         // we find the printouts on the list 'Customers Database'
-        Customer_Bank customer_bank_ = new Customer_Bank();
-        customer_bank_.find_customer("2");
-        customer_bank_.find_customer("4");
+        Customer_Bank customer_bank = new Customer_Bank();
+        customer_bank.find_customer("2");
+        customer_bank.find_customer("4");
 
         // 3. then, we add the accounts to new customers, and we find printout on the 'List of customers and accounts'
-        customer_bank_.find_customer_and_add_account("1", "1111");
-        customer_bank_.find_customer_and_add_account("2", "2222");
-        customer_bank_.find_customer_and_add_account("3", "3333");
-        customer_bank_.find_customer_and_add_account("4", "4444");
-        customer_bank_.find_customer_and_add_account("5", "5555");
+        customer_bank.find_customer_and_add_account("1", "1111");
+        customer_bank.find_customer_and_add_account("2", "2222");
+        customer_bank.find_customer_and_add_account("3", "3333");
+        customer_bank.find_customer_and_add_account("4", "4444");
+        customer_bank.find_customer_and_add_account("5", "5555");
 
         // 4. we check what the customer database looks like after adding the accounts, see printout 'Customers Database'
-        customer_bank_.find_customer("3");
-        customer_bank_.find_customer("1");
-        customer_bank_.find_customer("5");
+        customer_bank.find_customer("3");
+        customer_bank.find_customer("1");
+        customer_bank.find_customer("5");
 
+        // 5. customers deposit funds into their accounts, see printout 'List of customers and funds in the accounts'
+        customer_bank.find_customer_and_perform_operation_deposit_withdrawal_on_account("1", "1000000");
+        customer_bank.find_customer_and_perform_operation_deposit_withdrawal_on_account("2", "2000000");
+        customer_bank.find_customer_and_perform_operation_deposit_withdrawal_on_account("3", "3000000");
+        customer_bank.find_customer_and_perform_operation_deposit_withdrawal_on_account("4", "4000000");
+        customer_bank.find_customer_and_perform_operation_deposit_withdrawal_on_account("5", "5000000");
+
+        // 6. we randomly check the bank's customers again, see printout 'Customers Database'
+        customer_bank.find_customer("3");
+        customer_bank.find_customer("1");
+        customer_bank.find_customer("5");
 
 
     }
