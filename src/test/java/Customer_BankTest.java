62bea4a312f75f34669462bb93675ea62834eb4c
Karcz Przemysław <pkarc@PGI.LOCAL>
2022-06-22 15:05:28 +0200

    described the Customer_Bank class tests

--- src/test/java/Customer_BankTest.java
+++ src/test/java/Customer_BankTest.java
@@ -1,5 +1,4 @@
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -9,9 +8,11 @@ public class Customer_BankTest {
     @Test
     public void find_customer_test_01(){
 
+        //given, when
         Customer customer = new Customer();
         customer.add_new_customer("11", "E. Eleventh");
 
+        //then
         ArrayList<Object> bank_customer_list = Bank.getBank_customer_list();
 
         String[] next = new String[0];
@@ -29,9 +30,11 @@ public class Customer_BankTest {
     @Test
     public void find_customer_test_02(){
 
+        //given, when
         Customer customer = new Customer();
         customer.add_new_customer("12", "T. Twelfth");
 
+        //then
         ArrayList<Object> bank_customer_list = Bank.getBank_customer_list();
 
         String[] next = new String[0];
@@ -49,12 +52,15 @@ public class Customer_BankTest {
     @Test
     public void find_customer_and_add_account_test_01(){
 
+        //given
         Customer customer = new Customer();
         customer.add_new_customer("3333", "T. Three thousand three hundred and thirty-third");
 
+        //when
         Customer_Bank customer_bank = new Customer_Bank();
         customer_bank.find_customer_and_add_account("3333", "3333");
 
+        //then
         ArrayList<Object> bank_customer_list = Bank.getBank_customer_list();
 
         String[] next = new  String[0];
@@ -72,12 +78,15 @@ public class Customer_BankTest {
     @Test
     public void find_customer_and_add_account_test_02(){
 
+        //given
         Customer customer = new Customer();
         customer.add_new_customer("3334", "T. Three thousand three hundred and thirty-fourth");
 
+        //when
         Customer_Bank customer_bank = new Customer_Bank();
         customer_bank.find_customer_and_add_account("3334", "3334");
 
+        //then
         ArrayList<Object> bank_customer_list = Bank.getBank_customer_list();
 
         String[] next = new  String[0];
@@ -95,13 +104,17 @@ public class Customer_BankTest {
     @Test
     public void find_customer_and_perform_operation_deposit_withdrawal_from_account_test_01(){
 
+        //given
         Customer customer = new Customer();
         customer.add_new_customer("10001", "T. Ten thousand first");
 
         Customer_Bank customer_bank = new Customer_Bank();
         customer_bank.find_customer_and_add_account("10001", "10001");
+
+        //when
         customer_bank.find_customer_and_perform_operation_deposit_withdrawal_from_account("10001", "10000001");
 
+        //then
         ArrayList<Object> bank_customer_list = Bank.getBank_customer_list();
 
         String[] next = new String[0];
@@ -119,13 +132,17 @@ public class Customer_BankTest {
     @Test
     public void find_customer_and_perform_operation_deposit_withdrawal_from_account_test_02(){
 
+        //given
         Customer customer = new Customer();
         customer.add_new_customer("999", "N. Nine hundred and ninety-ninth");
 
         Customer_Bank customer_bank = new Customer_Bank();
         customer_bank.find_customer_and_add_account("999", "9999");
+
+        //when
         customer_bank.find_customer_and_perform_operation_deposit_withdrawal_from_account("999", "999999");
 
+        //then
         ArrayList<Object> bank_customer_list = Bank.getBank_customer_list();
 
         String[] next = new String[0];
@@ -143,6 +160,7 @@ public class Customer_BankTest {
     @Test
     public void find_customers_and_make_transfers_between_customers_test_01(){
 
+        //given
         Customer customer = new Customer();
         customer.add_new_customer("1234", "F. 123-fourth");
         customer.add_new_customer("1235", "F. 123-fifth");
@@ -154,9 +172,11 @@ public class Customer_BankTest {
         customer_bank.find_customer_and_perform_operation_deposit_withdrawal_from_account("1234", "400");
         customer_bank.find_customer_and_perform_operation_deposit_withdrawal_from_account("1235", "500");
 
+        //when
         customer_bank.find_customers_and_make_transfers_between_customers("1234", "-400",
                 "1235");
 
+        //then
         ArrayList<Object> bank_customer_list = Bank.getBank_customer_list();
 
         String[] next = new String[0];
@@ -174,6 +194,7 @@ public class Customer_BankTest {
     @Test
     public void find_customers_and_make_transfers_between_customers_test_02(){
 
+        //given
         Customer customer = new Customer();
         customer.add_new_customer("12345678", "F. 1234567-eighth");
         customer.add_new_customer("12345679", "F. 1234567-ninth");
@@ -185,9 +206,11 @@ public class Customer_BankTest {
         customer_bank.find_customer_and_perform_operation_deposit_withdrawal_from_account("12345678", "1000");
         customer_bank.find_customer_and_perform_operation_deposit_withdrawal_from_account("12345679", "2000");
 
+        //when
         customer_bank.find_customers_and_make_transfers_between_customers("12345679", "-2000",
                 "12345678");
 
+        //then
         ArrayList<Object> bank_customer_list = Bank.getBank_customer_list();
 
         String[] next = new String[0];
@@ -205,12 +228,15 @@ public class Customer_BankTest {
     @Test
     public void edit_other_customer_details_Other_Data_test_01(){
 
+        //given
         Customer customer = new Customer();
         customer.add_new_customer("111555777", "F. Seventh");
 
+        //when
         Customer_Bank customer_bank = new Customer_Bank();
         customer_bank.edit_other_customer_details_Other_Data("111555777", "Mob: 999-888-777");
 
+        //then
         ArrayList<Object> bank_customer_list = Bank.getBank_customer_list();
 
         String[] next = new String[0];
@@ -228,12 +254,15 @@ public class Customer_BankTest {
     @Test
     public void edit_other_customer_details_Other_Data_test_02(){
 
+        //given
         Customer customer = new Customer();
         customer.add_new_customer("111555777222", "F. Second");
 
+        //when
         Customer_Bank customer_bank = new Customer_Bank();
         customer_bank.edit_other_customer_details_Other_Data("111555777222", "Mob: 999-888-222");
 
+        //then
         ArrayList<Object> bank_customer_list = Bank.getBank_customer_list();
 
         String[] next = new String[0];
@@ -251,12 +280,15 @@ public class Customer_BankTest {
     @Test
     public void edit_personal_customer_details_Name_test_01(){
 
+        //given
         Customer customer = new Customer();
         customer.add_new_customer("999888777", "N. Ninth-Eighth-Seventh");
 
+        //when
         Customer_Bank customer_bank = new Customer_Bank();
         customer_bank.edit_personal_customer_details_Name("999888777", "N. Seventh");
 
+        //then
         ArrayList<Object> bank_customer_list = Bank.getBank_customer_list();
 
         String[] next = new String[0];
